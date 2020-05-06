4b6ab56d0127ce66ff8011f531dcc7d7ac41a6ae
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-05-06 23:32:41 +0530

    Refactor. Adding default and Parametrized Constructors.

--- src/main/java/com/bridgelabz/MaximumOfThree.java
+++ src/main/java/com/bridgelabz/MaximumOfThree.java
@@ -5,6 +5,16 @@ public class MaximumOfThree<E extends Comparable<E>> {
     E secondInput;
     E thirdInput;
 
+    public MaximumOfThree() {
+
+    }
+    
+    public MaximumOfThree(E firstInput, E secondInput, E thirdInput) {
+        this.firstInput=firstInput;
+        this.secondInput=secondInput;
+        this.thirdInput=thirdInput;
+    }
+
     public <E extends Comparable> E findMaxOfThree(E firstInput, E secondInput, E thirdInput) {
 
         E maximum=firstInput;
