27e69ca38b70304dfe899b87e60ef898d1dbf8b1
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-05-07 00:49:33 +0530

    Adding feature to Print Max to StdOut using Generic Method.

--- src/main/java/com/bridgelabz/MaximumOfThree.java
+++ src/main/java/com/bridgelabz/MaximumOfThree.java
@@ -17,7 +17,11 @@ public class MaximumOfThree<E extends Comparable<E>> {
 
         Arrays.sort(input);
         int maximumIndex=input.length-1;
+        MaximumOfThree.printMax(input[maximumIndex]);
         return input[maximumIndex];
     }
 
+    public static <E> void printMax(E maxResult) {
+        System.out.println("Maximum among all input is: "+maxResult);
+    }
 }
