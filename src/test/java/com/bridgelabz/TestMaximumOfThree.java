2c2c029fe5daf35cbb1bd6602f124a8c61a0a45d
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-05-06 21:10:11 +0530

    Add. Adding a new feature to check 1st position Float as Max.

--- src/test/java/com/bridgelabz/TestMaximumOfThree.java
+++ src/test/java/com/bridgelabz/TestMaximumOfThree.java
@@ -28,4 +28,12 @@ public class TestMaximumOfThree {
         int checkMaxInteger = maximumOfThree.findMaxInteger(20,150,255);
         Assert.assertEquals(255,checkMaxInteger);
     }
+
+    @Test
+    public void givenThreeInput_maxFloatAtFirstPosition_shouldReturnMaximum()
+    {
+        MaximumOfThree maximumOfThree = new MaximumOfThree();
+        Float checkMaxFloat = maximumOfThree.findMaxFloat(20.2f,10.0f,12.65f);
+        Assert.assertEquals(java.util.Optional.of((Float)20.2f), java.util.Optional.of(checkMaxFloat));
+    }
 }
